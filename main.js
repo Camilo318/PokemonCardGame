323212b66021ffe061f3ea9416e9b243b5c6aeeb
Camilo318 <camiloa0318@gmail.com>
2020-05-02 09:30:53 -0500

    New backgroundColor, sweet alerts added, event listener for the DOM added, song added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', swal('Billie', 'When we all fall asleep, where do we go?'))
+
 const cards = document.querySelectorAll('.game__card')
 
 cards.forEach(card => card.addEventListener('click', flip))
