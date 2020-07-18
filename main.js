f9b6dae28916a843814db26be24a5a9ff888a167
Camilo318 <camiloa0318@gmail.com>
2020-07-17 21:33:17 -0500

    Main logic done, game fully playable

--- main.js
+++ main.js
@@ -34,25 +34,113 @@ class Game {
         //Instanciamos la clase AudioController
         this.audioController = new AudioController()
     }
-    startGame() {
+    startGame() { //Starting a new game
         this.cardToCheck = null
         this.totalClicks = 0
         this.timeRemaining = this.Totaltime
         this.matchedCards = []
         this.busy = true
-        this.shuffleCards()
+
+        setTimeout(() => {
+            this.shuffleCards()
+            this.audioController.startMusic()
+            this.countDown = this.startCountDown()
+            this.busy = false
+        }, 500)
+
+        this.flips.innerText = this.totalClicks
+        this.timer.innerText = this.timeRemaining
+        this.hideCards()
+    }
+
+
+    startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--
+            this.timer.innerText = this.timeRemaining
+            if (this.timeRemaining < 1) {
+                this.gameOver()
+            }
+        }, 1000)
+    }
+
+    gameOver() {
+        clearInterval(this.countDown)
+        this.audioController.stopMusic()
+        swal('Sorry', 'Better luck next time', 'error')
+            .then(() => {
+                this.startGame()  //Reseting the game after we lose
+            })
     }
 
     flipCard(card) {
         if (this.canFlipCard(card)) {
-            card.classList.toggle('visible')
+            card.classList.add('visible')
             this.totalClicks++
             this.flips.innerText = this.totalClicks
+            if (this.cardToCheck) {
+                this.checkForMatch(card)
+            } else {
+                this.cardToCheck = card
+            }
         }
     }
 
+    checkForMatch(card) {
+        if (this.getCardValue(card) === this.getCardValue(this.cardToCheck)) {
+            // console.log('It is a match')
+            this.cardsMatch(card, this.cardToCheck)
+        } else {
+            this.cardsMisMatch(card, this.cardToCheck)
+        }
+        
+        this.cardToCheck = null
+    }
+
+    getCardValue(card) {
+        //targeando el src de la imagen dentro de .card__front
+        return card.querySelector('.card__front .card__value').src
+    }
+
+    cardsMatch(card1,card2) {
+        this.matchedCards.push(card1)
+        this.matchedCards.push(card2)
+        card1.classList.add('matched')
+        card2.classList.add('matched')
+        if (this.matchedCards.length == this.cards.length) {
+            this.victory()
+        }
+    }
+
+    victory() {
+        clearInterval(this.countDown)
+        this.audioController.stopMusic()
+        swal('You did it my friend! 😼😼',
+         `You truly are Pokemon Master! \nTime Remaining: ${this.timeRemaining}\nFlips: ${this.totalClicks}`, 
+         'success').then(() => {
+             this.startGame()
+         })
+    }
+
+    cardsMisMatch(card1, card2) {
+        this.busy = true
+        //Giving time to the user to remember the image
+        setTimeout(() => {
+            card1.classList.remove('visible')
+            card2.classList.remove('visible')
+            this.busy = false
+        }, 1000)
+    }
+
+    hideCards() {
+        this.cards.forEach(card => {
+            card.classList.remove('visible')
+            card.classList.remove('matched')
+        })
+    }
+
     shuffleCards() {
-        for (let i = this.cards.length-1; i >=0; i--) {
+        for (let i = this.cards.length-1; i >= 0; i--) {
             let randomIndex = Math.floor(Math.random() * (i+1))
             this.cards[randomIndex].style.order = i;
             this.cards[i].style.order = randomIndex
@@ -60,9 +148,7 @@ class Game {
     }
 
     canFlipCard(card) {
-        // return (!this.busy && !this.matchedCards.includes(card) && card !== cardToCheck)
-        return true
-
+        return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
     }
 }
 
@@ -74,9 +160,9 @@ document.addEventListener('DOMContentLoaded', ready)
 
 
 function ready() {
-    swal('🙀', 'Go ahead and catch em all')
+    swal('Go ahead and catch \'em all 🙀', 'You\'ve got 100 seconds, hurry up!', 'warning')
         .then(() => {
-            const game = new Game(100, cards)  //Instanciamos la clase Game
+            const game = new Game(100, cards)  //Instanciamos la clase Game (tiempo, arreglo de cartas)
             game.startGame()  //Ejecutamos el metodo que inicia el juego
             
             cards.forEach(card => {
