c4408cf01bf5b0e97717ec102d8f26b1551c20d9
Camilo318 <camiloa0318@gmail.com>
2022-11-06 16:04:37 -0500

    Refactor alerts
    * Add object with button config
    * Add will-change

--- main.js
+++ main.js
@@ -4,23 +4,34 @@ async function ready() {
   const cards = Array.from(document.querySelectorAll('.game__card'))
   const gameContainer = document.querySelector('.game')
 
-  await swal(
-    "Go ahead and catch 'em all 🙀",
-    "You've got 80 seconds pal, hurry up!",
-    'warning',
-    {
-      button: "Let's go!"
+  await swal({
+    title: "Go ahead and catch 'em all 🙀",
+    text: "You've got 80 seconds pal, hurry up!",
+    icon: 'warning',
+    buttons: {
+      confirm: {
+        text: "Let's go",
+        value: true
+      }
     }
-  )
+  })
 
-  const playSound = await swal(
-    'Sound Options',
-    'Would you like the game to play sound?',
-    'info',
-    {
-      buttons: ['No, do not play sound', 'Yes, play sound']
+  const playSound = await swal({
+    title: 'Sound Options',
+    text: 'Would you like the game to play sound?',
+    icon: 'info',
+    buttons: {
+      cancel: {
+        value: null,
+        text: 'No, do not play sound',
+        visible: true
+      },
+      confirm: {
+        value: true,
+        text: 'Yes, play sound'
+      }
     }
-  )
+  })
   const vol = playSound ? 0.5 : 0
   const game = new Game(80, cards, vol)
   game.startGame()
