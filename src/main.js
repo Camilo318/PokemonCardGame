bd4488c1a254c92e2c0223992af072d82ffeb0a8
Camilo318 <camiloa0318@gmail.com>
2022-11-17 17:14:29 -0500

    Add sweetalert2
    * Refactoring of alerts
    * Add callbacks for victory and game over events

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Game from './Game'
-import swal from 'sweetalert'
+import Swal from 'sweetalert2'
 
 document.addEventListener('DOMContentLoaded', ready)
 
@@ -7,36 +7,61 @@ async function ready() {
   const cards = Array.from(document.querySelectorAll('.game__card'))
   const gameContainer = document.querySelector('.game')
 
-  await swal({
+  await Swal.fire({
     title: "Go ahead and catch 'em all 🙀",
     text: "You've got 80 seconds pal, hurry up!",
     icon: 'warning',
-    buttons: {
-      confirm: {
-        text: "Let's go",
-        value: true
-      }
-    }
+    confirmButtonText: "Let's go",
+    allowOutsideClick: false
   })
 
-  const playSound = await swal({
+  const playSound = await Swal.fire({
     title: 'Sound Options',
     text: 'Would you like the game to play sound?',
     icon: 'info',
-    buttons: {
-      cancel: {
-        value: null,
-        text: 'No, do not play sound',
-        visible: true
-      },
-      confirm: {
-        value: true,
-        text: 'Yes, play sound'
-      }
-    }
+    showDenyButton: true,
+    confirmButtonText: 'Yes, play sound',
+    denyButtonText: `No, do not play sound`,
+    allowOutsideClick: false
   })
-  const vol = playSound ? 0.5 : 0
-  const game = new Game(80, cards, vol)
+
+  const vol = playSound.isConfirmed ? 0.5 : 0
+
+  const gameOverCb = async () => {
+    const playerChoice = await Swal.fire({
+      title: 'Sorry 😿',
+      text: 'Better luck next time pal',
+      icon: 'error',
+      confirmButtonText: 'Retry',
+      allowOutsideClick: false
+    })
+
+    return playerChoice.isConfirmed
+  }
+
+  const victoryCb = async (timeRemaining, totalClicks) => {
+    const playerChoice = await Swal.fire({
+      title: 'You truly are a Pokemon Master! 😼',
+      html: `You did it my friend!<br>
+      Time Remaining: ${timeRemaining}<br>
+      Flips: ${totalClicks}`,
+      icon: 'success',
+      confirmButtonText: 'Play again',
+      showDenyButton: true,
+      denyButtonText: 'Exit',
+      allowOutsideClick: false
+    })
+
+    return playerChoice.isConfirmed
+  }
+
+  const game = new Game({
+    time: 60,
+    cards,
+    vol,
+    gameOverCb,
+    victoryCb
+  })
   game.startGame()
   gameContainer.addEventListener('click', e => {
     const gameCard = e.target.closest('.game__card')
